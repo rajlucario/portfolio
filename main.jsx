86d18fc2e55a84f6322bff875eda87a477215c48
rajlucario <rajnarayananabc@gmail.com>
2025-10-06 10:40:25 +0530

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import RajPortfolio from './rajportfolio.jsx' // Note the new file name and path
+import './index.css' // Your default CSS file
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RajPortfolio />
+  </React.StrictMode>,
+)
