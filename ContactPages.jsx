b0446b0a5844279b4de0eac09ced6faefe5f01ad
rajlucario <rajnarayananabc@gmail.com>
2025-10-06 10:43:35 +0530

    Create ContactPages.jsx

---
+++ ContactPages.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ContactSection from '../components/ContactSection';
+const contactImageUrl = "https://placehold.co/800x600/1f2937/f3f4f6?text=Lets+Work";
+const ContactPage = () => <ContactSection contactImageUrl={contactImageUrl} />;
+export default ContactPage;
